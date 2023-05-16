6e19b50f3fdf241af493afb8ffd6ea2db2e35370
Connor Swanson <swanscon@gmail.com>
2023-05-15 22:13:20 -0500

    Results page calculating

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,34 @@
 <script setup>
   import { RouterView } from 'vue-router';
+  import { storeToRefs, mapActions } from 'pinia';
   import { useCalcStore } from '../stores/calculator';
   const calc = useCalcStore();
+  const { maxLoad, curLoad, status } = storeToRefs(calc);
+  const { reset } = mapActions(useCalcStore, ['reset'])
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>{{ calc.maxLoad }}</h1>
-    <RouterLink to="/"><button>RESET &#8634;</button></RouterLink>
+    <!-- <h1>{{ calc.maxLoad }}</h1> -->
+      <h4 v-if="maxLoad!='' && curLoad==''">
+				Your maximum equip load is {{ maxLoad }}
+			</h4>
+			<p v-if="curLoad==''">
+				<!-- LEAVE EMPTY -->
+			</p>
+      <h4 v-else-if="curLoad<=(maxLoad*.2999)">
+        {{ status }} LIGHT.
+      </h4>
+      <h4 v-else-if="curLoad>(maxLoad*.2999) && curLoad<=(maxLoad*.6999)">
+        {{ status }} MEDIUM.
+      </h4>
+      <h4 v-else-if="curLoad>(maxLoad*.6999) && curLoad<=(maxLoad*.9999)">
+        {{ status }} HEAVY.
+      </h4>
+      <h4 v-else-if="curLoad>=(maxLoad*.9999)">
+        {{ status }} OVERLOADED.
+      </h4>
+    <RouterLink to="/"><button @click="reset">RESET &#8634;</button></RouterLink>
   </div>
     <RouterView/>
 </template>
